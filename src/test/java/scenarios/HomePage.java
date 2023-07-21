5706767610cb1a0d06d7ab926afcee69acad8c92
Hasan Ozyer <hasanozyer06@gmail.com>
2023-07-21 11:00:42 +0300

    "Added methods for login, clicking on menus, and searching on the 'HomePage' class to interact with the web application and perform actions based on the given parameters."

--- src/test/java/scenarios/HomePage.java
+++ src/test/java/scenarios/HomePage.java
@@ -2,8 +2,15 @@ package scenarios;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import utility.Browsers;
 import utility.Driver;
+
+import java.text.MessageFormat;
+import java.time.Duration;
+import java.util.List;
 
 public class HomePage {
 
@@ -11,28 +18,65 @@ public class HomePage {
     private WebDriverWait wait;
 
     public HomePage() {
-        driver = Driver.getDriver();
+        this(Browsers.CHROME);
     }
 
-    public HomePage(WebDriver driver) {
-        this.driver = driver;
+    public HomePage(Browsers browser) {
+        this.driver = Driver.getDriver(browser);
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
     }
 
     String url = "https://demowebshop.tricentis.com/";
 
-    String sMainMenuLinks = "//div[@class='header-links']//a[contains(.,'Log in')]";
+    String sMainMenuLinks = "//div[@class=\"header-links\"]//a[contains(.,\"{0}\")]";
+    By lLoginFormUserName = By.id("Email");
+    By lLoginFormPassword = By.id("Password");
+    By lLoginSubmitButton = By.cssSelector("input[value='Log in']");
+    By lSearchInput = By.id("small-searchterms");
+    By lSearchButton = By.cssSelector("input[value='Search']");
+    By lSearchedProducts = By.cssSelector(".search-results .item-box");
 
     public void open() {
         driver.get(url);
     }
 
+    public By getLocatorMenuOf(String text) {
+        return By.xpath(MessageFormat.format(sMainMenuLinks, text));
+
+    }
+
     public boolean login(String username, String password) {
-        return false;
+        By menuLocator = getLocatorMenuOf("Log in");
+        click(menuLocator);
+        sendKeys(lLoginFormUserName, username);
+        sendKeys(lLoginFormPassword, password);
+        click(lLoginSubmitButton);
+
+        int x = driver.findElements(getLocatorMenuOf("Log out")).size();
+        return x >= 1;
     }
 
     public void clickMenu(String menuText) {
-
+        By menuLocator = getLocatorMenuOf(menuText);
+        click(menuLocator);
     }
 
+    public void click(By locator) {
+        wait.until(ExpectedConditions.elementToBeClickable(locator))
+                .click();
+    }
+
+    public void sendKeys(By locator, String textToSend) {
+        wait.until(ExpectedConditions.visibilityOfElementLocated(locator))
+                .sendKeys(textToSend);
+    }
+
+    public List<WebElement> search(String textToSearch) {
+        sendKeys(lSearchInput, textToSearch);
+        click(lSearchButton);
+        wait.until(ExpectedConditions.titleContains("Search"));
+//        wait.until(ExpectedConditions.numberOfElementsToBeMoreThan(lSearchedProducts, 0));
+        return driver.findElements(lSearchedProducts);
+    }
 
 }
