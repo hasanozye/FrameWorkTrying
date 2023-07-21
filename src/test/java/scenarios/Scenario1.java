62f6e3f672963848ecb9ddfbc0a35537d1a80a9c
Hasan Ozyer <hasanozyer06@gmail.com>
2023-07-21 11:01:34 +0300

    "Implemented test scenarios using the 'HomePage' class to perform login, search for products, and assert the number of listed products and 'Add to Cart' elements. The scenarios were executed successfully for different browsers (Chrome and Edge)."

--- src/test/java/scenarios/Scenario1.java
+++ src/test/java/scenarios/Scenario1.java
@@ -1,12 +1,19 @@
 package scenarios;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import utility.Browsers;
 import utility.Driver;
 
+import java.util.List;
+
 public class Scenario1 {
-    WebDriver driver = Driver.getDriver();
+    WebDriver driver;
+    String myUsername = "abc123@aa.com";
+    String myPassword = "123456";
 
 
     @Test
@@ -22,9 +29,28 @@ public class Scenario1 {
          3. desktop aratin ve 6 ürün listelendigini assert edin
          4. "Add to Cart" içeren ürün sayisini veren güzel bir xpath yazmalisiniz
           */
-        driver = Driver.getDriver(Browsers.EDGE);
-
         HomePage homePage = new HomePage();
+        driver = Driver.getDriver();
+        homePage.open();
+        System.out.println(homePage.login(myUsername, myPassword));
+
+        List<WebElement> searchedProducts = homePage.search("comp");
+        int prods = searchedProducts.size();
+        Assert.assertEquals(prods, 7);
+
+        int addToCart = driver.findElements(By.cssSelector(".search-results .item-box input")).size();
+        Assert.assertEquals(addToCart, 6);
+
+
+        Driver.getDriver().quit();
+    }
+
+    @Test
+    public void test2() {
+        HomePage hp = new HomePage(Browsers.EDGE);
+        hp.open();
+        hp.login(myUsername, myPassword);
+        Driver.getDriver().quit();
     }
 
 
